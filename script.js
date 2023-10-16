7eeff54207f7f67a3d584687f8d303ecc3e2c098
marekAren2 <marekaren83@gmail.com>
2023-10-16 22:56:11 +0200

    last vsc 2023_10_16

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 
 // const Ksiazka = function(tytul, autor, przeczytana = false) {
-class Ksiazka  {
-    constructor (tytul, autor, przeczytana = false) {
+class Ksiazka {
+    constructor(tytul, autor, przeczytana = false) {
     this.tytul = tytul;
     this.autor = autor;
     this.przeczytana = przeczytana;
     }
     // this.opiszKsiazke = function() {
+    
     opiszKsiazke() {
     console.log(`Książka ma tytuł ${this.tytul}, autorem jest ${this.autor}`)
     if (this.przeczytana === true) {
       console.log(`i została przeczytana`);
     // } else { console.log(`i nie została przeczytana`); 
-      };
-    };
-};
+      }
+    }
+}
 
 function iloscPrzeczytanych(tablica) {
   let iloscPrzeczytanych = 0;
@@ -43,12 +44,10 @@ let biblioteka = [ksiazka1, ksiazka2, ksiazka3];
 // console.log(biblioteka);
 console.log(biblioteka.length);
 
-for (i=0;i< biblioteka.length;i++) {
-  
+for (let i = 0; i < biblioteka.length; i++) {
   console.log(biblioteka[i]);
   biblioteka[i].opiszKsiazke();
-  
-} 
+}
 
 iloscPrzeczytanych(biblioteka);
 
@@ -62,11 +61,18 @@ console.log("🚀 ~ iloscPrzeczytanych(biblioteka):", iloscPrzeczytanych(bibliot
 // }
 
 
-/* Napisz klasę "Ksiazka", która ma właściwości "tytul", "autor", "przeczytana" (przeczytana jest typu Boolean) oraz jedną metodę "opiszKsiazke", która zwraca stringa opisującego tą książkę wg. wzoru "Książka ma tytuł Wiedźmin, autorem jest Andrzej Sapkowski i została przeczytana.".
+/* Napisz klasę "Ksiazka", która ma właściwości "tytul", "autor", "przeczytana" 
+(przeczytana jest typu Boolean) oraz jedną metodę "opiszKsiazke", która zwraca stringa
+ opisującego tą książkę wg. wzoru "Książka ma tytuł Wiedźmin, autorem jest 
+ Andrzej Sapkowski i została przeczytana.".
 
-Stwórz 3 instancje obiektu tej klasy z czego jedna z książek ma być już przeczytana, a następnie stwórz nową tablicę globalną i dodaj do niej te 3 książki.
+Stwórz 3 instancje obiektu tej klasy z czego jedna z książek ma być już przeczytana, 
+a następnie stwórz nową tablicę globalną i dodaj do niej te 3 książki.
 
-Następnie napisz funkcję "iloscPrzeczytanych", która jako parametr przyjmuje tablicę obiektów (naszych książek, ktore stworzyliśmy), a zwraca nam ilość książek, ktore zostały przeczytane. Podczas iteracji po tablicy (wewnątrz tej funkcji) i sprawdzania, czy książka jest przeczytana, wywołuj także na obiekcie metodę opiszKsiazke(), a wynik jej działania wyświetlaj w konsoli.
+Następnie napisz funkcję "iloscPrzeczytanych", która jako parametr przyjmuje tablicę obiektów
+ (naszych książek, ktore stworzyliśmy), a zwraca nam ilość książek, ktore zostały przeczytane. 
+ Podczas iteracji po tablicy (wewnątrz tej funkcji) i sprawdzania, czy książka jest przeczytana, 
+ wywołuj także na obiekcie metodę opiszKsiazke(), a wynik jej działania wyświetlaj w konsoli.
 
 Na samym końcu wyświetl w konsoli ilość przeczytanych książek
 
